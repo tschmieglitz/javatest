8970086383f4856aefdd6cba82567ae6e03b3a22
tschmieglitz <thomas@schmieglitz.de>
2023-06-18 21:02:02 +0200

    Test

--- Berechnung.java
+++ Berechnung.java
@@ -1,6 +1,6 @@
 public class Berechnung {
     public static void main(String[] args) {
-        Werte werte = new Werte(5, 10);
+        Werte werte = new Werte(9, 10);
         int ergebnis = werte.zahl1 + werte.zahl2;
 
         System.out.println("Die Summe von " + werte.zahl1 + " und " + werte.zahl2 + " ist: " + ergebnis);
