a2658e313ff9b37de89d4d0042c12165eae066e4
tschmieglitz <thomas@schmieglitz.de>
2023-06-18 20:57:24 +0200

    Test

---
+++ Berechnung.java
@@ -0,0 +1,8 @@
+public class Berechnung {
+    public static void main(String[] args) {
+        Werte werte = new Werte(5, 10);
+        int ergebnis = werte.zahl1 + werte.zahl2;
+
+        System.out.println("Die Summe von " + werte.zahl1 + " und " + werte.zahl2 + " ist: " + ergebnis);
+    }
+}
---
+++ Werte.java
@@ -0,0 +1,8 @@
+public class Werte {
+    public int zahl1;
+    public int zahl2;
+    public Werte(int zahl1, int zahl2) {
+        this.zahl1 = zahl1;
+        this.zahl2 = zahl2;
+    }
+}
